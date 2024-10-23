a3a0bfb181d1a2d8155d017414b27391231158b5
daniela <dggtn97@gmail.com>
2024-10-23 16:31:26 -0300

    mapeo los elementos que quiero usar

---
+++ 2086-screenmatch-front-end-main/teste.js
@@ -0,0 +1,8 @@
+import getDatos from "./getDatos.js";
+
+// Mapea los elementos DOM que desea actualizar
+const elementos = {
+    top5: document.querySelector('[data-name="top5"]'),
+    lancamentos: document.querySelector('[data-name="lanzamientos"]'),
+    series: document.querySelector('[data-name="series"]')
+};
