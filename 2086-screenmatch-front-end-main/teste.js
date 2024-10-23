25dbc49e82d6f08177be8c63053b1272401e6cd3
daniela <dggtn97@gmail.com>
2024-10-23 16:32:11 -0300

    agrego funcion para crear la lista de peliculas

--- 2086-screenmatch-front-end-main/teste.js
+++ 2086-screenmatch-front-end-main/teste.js
@@ -6,3 +6,26 @@ const elementos = {
     lancamentos: document.querySelector('[data-name="lanzamientos"]'),
     series: document.querySelector('[data-name="series"]')
 };
+// Funcion para crear la lista de peliculas 
+function crearListaPeliculas(elemento, dados) {
+    // Verifica si hay un elemento <ul> dentro de la seccion
+    const ulExistente = elemento.querySelector('ul');
+
+    // Si ya existe un elemento <ul> dentro de la sección, elimínelo
+    if (ulExistente) {
+        elemento.removeChild(ulExistente);
+    }
+
+    const ul = document.createElement('ul');
+    ul.className = 'lista';
+    const listaHTML = dados.slice(0, 5).map((pelicula) => `
+        <li>
+            <a href="/detalles.html?id=${pelicula.id}">
+                <img src="${pelicula.poster}" alt="${pelicula.titulo}">
+            </a>
+        </li>
+    `).join('');
+
+    ul.innerHTML = listaHTML;
+    elemento.appendChild(ul);
+}
